d1059c96a13355fe27af9c12d034d79adf20fec7
Mykola Shitik <shitik@artistsoft.com>
2014-09-14 20:11:30 +0300

    Fixed displaying schedule

--- app/libs/database.go
+++ app/libs/database.go
@@ -1,6 +1,7 @@
 package db_lib
 
 import (
+	"fmt"
 	_ "fmt"
 	"github.com/coocood/qbs"
 	"rozklad_cdtu/app/models"
@@ -100,29 +101,50 @@ func FacultiesList(db *qbs.Qbs) []*models.Faculties {
 	return faculties
 }
 
-func GroupSchedule(db *qbs.Qbs, group_id int64, days []*models.Days, pairs []*models.Pairs) []json_models.Day {
-	var schedule []*models.Schedule
+func EmptySchedule(days []*models.Days, pairs []*models.Pairs) []json_models.Day {
 	var days_out []json_models.Day
-
 	for j := range days {
 		var pairs_out []json_models.Pair
 		for i := range pairs {
-			pairs_out = append(pairs_out, json_models.Pair{pairs[i].Id, pairs[i].Number, "", "", 0})
+			pairs_out = append(pairs_out, json_models.Pair{pairs[i].Id, pairs[i].Number, "", "", 2})
 		}
 		days_out = append(days_out, json_models.Day{days[j].Id, days[j].Day, pairs_out})
 	}
+	return days_out
+}
+
+func GroupSchedule(db *qbs.Qbs, group_id int64, days []*models.Days, pairs []*models.Pairs) []json_models.Day {
+	var schedule []*models.Schedule
+	days_out := EmptySchedule(days, pairs)
 
 	err := db.WhereEqual("schedule.group_id", group_id).FindAll(&schedule)
 	if err == nil {
+		scheduleString := func(i int) string {
+			subject := schedule[i]
+			housing := new(models.Housings)
+			housing.Id = subject.Audience.HousingId
+			err := db.Find(housing)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(subject.Audience)
+			return subject.Subject.Subject + " - " +
+				subject.SubjectType + " " +
+				subject.Audience.Number + "-" +
+				housing.Number + " " +
+				subject.Teacher.Rank + " - " +
+				subject.Teacher.LastName + " " +
+				subject.Teacher.FirstName + " " +
+				subject.Teacher.MiddleName
+		}
 		for i := range schedule {
 			if schedule[i].PairType == "0" {
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = schedule[i].Subject.Subject
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = scheduleString(i)
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Type = 1
 			} else if schedule[i].PairType == "1" {
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = schedule[i].PairType
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Type = 1
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 += scheduleString(i)
 			} else if schedule[i].PairType == "2" {
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject2 = schedule[i].PairType
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Type = 1
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject2 += scheduleString(i)
 			}
 		}
 	}
@@ -132,27 +154,35 @@ func GroupSchedule(db *qbs.Qbs, group_id int64, days []*models.Days, pairs []*mo
 
 func TeacherSchedule(db *qbs.Qbs, teacher_id int64, days []*models.Days, pairs []*models.Pairs) []json_models.Day {
 	var schedule []*models.Schedule
-	var days_out []json_models.Day
-
-	for j := range days {
-		var pairs_out []json_models.Pair
-		for i := range pairs {
-			pairs_out = append(pairs_out, json_models.Pair{pairs[i].Id, pairs[i].Number, "", "", 0})
-		}
-		days_out = append(days_out, json_models.Day{days[j].Id, days[j].Day, pairs_out})
-	}
+	days_out := EmptySchedule(days, pairs)
 
 	err := db.WhereEqual("schedule.teacher_id", teacher_id).FindAll(&schedule)
 	if err == nil {
+		scheduleString := func(i int) string {
+			subject := schedule[i]
+			housing := new(models.Housings)
+			housing.Id = subject.Audience.HousingId
+			err := db.Find(housing)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(subject.Audience)
+			return subject.Subject.Subject + " - " +
+				subject.SubjectType + " " +
+				subject.Audience.Number + "-" +
+				housing.Number + " " +
+				subject.Group.ShortName + " - " +
+				fmt.Sprintf("%v", subject.Group.Year) + "курс"
+		}
 		for i := range schedule {
 			if schedule[i].PairType == "0" {
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = schedule[i].Subject.Subject
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = scheduleString(i)
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Type = 1
 			} else if schedule[i].PairType == "1" {
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = schedule[i].PairType
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Type = 1
+				fmt.Println("sdgs")
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject1 = scheduleString(i)
 			} else if schedule[i].PairType == "2" {
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject2 = schedule[i].PairType
-				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Type = 1
+				days_out[schedule[i].DayId].Pair[schedule[i].PairId].Subject2 = scheduleString(i)
 			}
 		}
 	}
