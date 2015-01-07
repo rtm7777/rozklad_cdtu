f3fab7e76b45af50ba3304c890af9b773541d478
nikolai.s <shitik@artistsoft.com>
2015-01-07 13:08:08 +0200

    Small changes

--- app/controllers/init.go
+++ app/controllers/init.go
@@ -2,11 +2,9 @@ package controllers
 
 import (
 	"github.com/revel/revel"
-	"rozklad_cdtu/app/qbsDB"
 )
 
 func init() {
-	revel.OnAppStart(func() { qbsDB.Setup() })
 	revel.OnAppStart(InitDB)
 	revel.InterceptMethod((*GormController).Begin, revel.BEFORE)
 	revel.InterceptMethod(Admin.checkUser, revel.BEFORE)
