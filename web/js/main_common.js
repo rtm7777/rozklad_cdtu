e4c2a53de54d68eac659b82c83c21cdd2a3ad327
nikolai <shitik@artistsoft.com>
2015-02-21 13:52:02 +0200

    Ported some templates

--- web/js/main_common.js
+++ web/js/main_common.js
@@ -1,11 +1,12 @@
 window.$ = window.jQuery = require('jquery');
 import "bootstrap";
 import "./libs/socketEvents";
+import "./views/contentView";
 import {getValue} from "./services/localStorage";
 
 console.log(getValue("lala"));
 
-var _ = require('underscore');
+import _ from 'underscore';
 
 
 _.each([1, 2, 3], function (n) {
--- web/js/views/contentView.js
+++ web/js/views/contentView.js
@@ -1,68 +1,65 @@
-define(['jquery',
-		'underscore',
-		'../config/config',
-		'text!templates/group.html',
-		'text!templates/task.html'
-], ($, _, config, groupTemplate, taskTemplate) => {
+import _ from 'underscore';
+import config from "../config/config";
+import groupTemplate from '../templates/group.html';
+import taskTemplate from '../templates/task.html';
 
-	var $contentDiv = $("#schedule_container");
-	var $taskDiv = $("#tasks_container");
+var $contentDiv = $("#schedule_container");
+var $taskDiv = $("#tasks_container");
 
-	return {
-		addGroup(group) {
-			$contentDiv.append(_.template(groupTemplate, {
-				group  : group.shortName,
-				groupId: group.id,
-				days   : config.days,
-				pairs  : config.pairs
-			}));
-		},
+export default {
+	addGroup(group) {
+		$contentDiv.append(_.template(groupTemplate, {
+			group  : group.shortName,
+			groupId: group.id,
+			days   : config.days,
+			pairs  : config.pairs
+		}));
+	},
 
-		addTask(task) {
-			$taskDiv.append(_.template(taskTemplate, {
-				id      : task.id,
-				subject : task.subject,
-				type    : task.type,
-				progress: task.progress
-			}));
-		},
+	addTask(task) {
+		$taskDiv.append(_.template(taskTemplate, {
+			id      : task.id,
+			subject : task.subject,
+			type    : task.type,
+			progress: task.progress
+		}));
+	},
 
-		setPair(pair) {
-			$("#" + pair.id).html("<div class='admin-sub'>" + pair.subject +"</div><hr class='admin-sub-separator'><div class='admin-sub'></div>");
-		},
+	setPair(pair) {
+		$("#" + pair.id).html("<div class='admin-sub'>" + pair.subject +"</div><hr class='admin-sub-separator'><div class='admin-sub'></div>");
+	},
 
-		setDropdownValue($dropdown, $element) {
-			$dropdown
-				.html($element.text() + ' <span class="caret"></span>')
-				.attr("data-filter-id", $element.attr("data-filter-id"));
-		},
+	setDropdownValue($dropdown, $element) {
+		$dropdown
+			.html($element.text() + ' <span class="caret"></span>')
+			.attr("data-filter-id", $element.attr("data-filter-id"));
+	},
 
-		showScheduleContent() {
-			$("#content_loader").addClass("hide");
-			$("#day_table_col, #pair_table_col, #schedule_container").removeClass("hide");
-		},
+	showScheduleContent() {
+		$("#content_loader").addClass("hide");
+		$("#day_table_col, #pair_table_col, #schedule_container").removeClass("hide");
+	},
 
-		hideScheduleContent() {
-			$("#content_loader").removeClass("hide");
-			$("#day_table_col, #pair_table_col, #schedule_container").addClass("hide");
-		},
+	hideScheduleContent() {
+		$("#content_loader").removeClass("hide");
+		$("#day_table_col, #pair_table_col, #schedule_container").addClass("hide");
+	},
 
-		setLoaderMessage(message) {
-			$("#content_loader").text(message);
-		},
+	setLoaderMessage(message) {
+		$("#content_loader").text(message);
+	},
 
-		showTasksContent() {
-			$("#tasks_loader").addClass("hide");
-			$("#tasks_container").removeClass("hide");
-		},
+	showTasksContent() {
+		$("#tasks_loader").addClass("hide");
+		$("#tasks_container").removeClass("hide");
+	},
 
-		hideTasksContent() {
-			$("#tasks_container").addClass("hide");
-			$("#tasks_loader").removeClass("hide");
-		},
+	hideTasksContent() {
+		$("#tasks_container").addClass("hide");
+		$("#tasks_loader").removeClass("hide");
+	},
 
-		setTasksMessage(message) {
-			$("#tasks_loader p").text(message);
-		}
-	};
-});
+	setTasksMessage(message) {
+		$("#tasks_loader p").text(message);
+	}
+};
